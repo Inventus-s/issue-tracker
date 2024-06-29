4052be6f0cf5eaa6d82410cd712050a6095b9e95
stkmukund <164354566+stkmukund@users.noreply.github.com>
2024-06-30 00:07:35 +0530

    ClientSide-Validation

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -2,26 +2,29 @@
 
 import { Callout, TextField } from '@radix-ui/themes'
 import React, { useState } from 'react';
-import { Button } from '@radix-ui/themes';
+import { Button, Text } from '@radix-ui/themes';
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import { Controller, useForm } from 'react-hook-form';
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { newIssueSchema } from '@/app/validationSchema';
+import { z } from 'zod';
 
-interface IssueForm {
-  title: string;
-  description: string;
-}
+type IssueForm = z.infer<typeof newIssueSchema>
 
 const NewIssuePage = () => {
   const [error, setError] = useState('');
   const router = useRouter();
-  const { register, control, handleSubmit } = useForm<IssueForm>();
+  const { register, control, handleSubmit, 
+    formState: { errors } } = useForm<IssueForm>({
+    resolver: zodResolver(newIssueSchema)
+  });
 
   return (
     <div className='max-w-xl'>
-     { error && <Callout.Root color='red' className='mb-3' >
+      {error && <Callout.Root color='red' className='mb-3' >
         <Callout.Text>{error}</Callout.Text>
       </Callout.Root>}
       <form className='space-y-3'
@@ -34,8 +37,12 @@ const NewIssuePage = () => {
           }
         })} >
         <TextField.Root placeholder='Title' {...register("title")} />
+       {errors.title && <Text color='red' as='p'>{errors.title.message}</Text>}
         <Controller name='description' control={control}
-          render={({ field }) => <SimpleMDE placeholder={'Description'} {...field} />} />
+          render={({ field }) => 
+          <SimpleMDE placeholder={'Description'} {...field} />} />
+       {errors.description && <Text color='red' as='p'>{errors.description.message}</Text>}
+
         <Button>Create New Issue</Button>
       </form>
     </div>
