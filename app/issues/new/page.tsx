920562d31a601990874667c15b3f3e5c4417220f
stkmukund <164354566+stkmukund@users.noreply.github.com>
2024-06-30 21:58:52 +0530

    refactor

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -25,22 +25,24 @@ const NewIssuePage = () => {
       resolver: zodResolver(newIssueSchema)
     });
 
+  const onSubmit = handleSubmit(async (data) => {
+    try {
+      setLoading(true);
+      await axios.post('/api/issues', data);
+      router.push('/issues');
+    } catch (error) {
+      setLoading(false);
+      setError("An Unexpected error occurred");
+    }
+  })
+
   return (
     <div className='max-w-xl'>
       {error && <Callout.Root color='red' className='mb-3' >
         <Callout.Text>{error}</Callout.Text>
       </Callout.Root>}
       <form className='space-y-3'
-        onSubmit={handleSubmit(async (data) => {
-          try {
-            setLoading(true);
-            await axios.post('/api/issues', data);
-            router.push('/issues');
-          } catch (error) {
-            setLoading(false);
-            setError("An Unexpected error occurred");
-          }
-        })} >
+        onSubmit={onSubmit} >
         <TextField.Root placeholder='Title' {...register("title")} />
         {<ErrorMessage>{errors.title?.message}</ErrorMessage>}
         <Controller name='description' control={control}
