67decc1c9f18191295ebb3a107f65d94ed812f83
stkmukund <164354566+stkmukund@users.noreply.github.com>
2024-07-16 15:45:33 +0530

    Delete issue API

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -8,22 +8,39 @@ export async function PATCH(
 ) {
   const body = await request.json();
   const validation = newIssueSchema.safeParse(body);
-  if(!validation.success) 
-    return NextResponse.json(validation.error.format(), { status: 400})
+  if (!validation.success)
+    return NextResponse.json(validation.error.format(), { status: 400 });
 
   const issue = await prisma.issue.findUnique({
-    where: {id: parseInt(params.id)}
-  })
-  if(!issue) 
-    return NextResponse.json({error: 'Invalid issue'},{status: 404})
+    where: { id: parseInt(params.id) },
+  });
+  if (!issue)
+    return NextResponse.json({ error: "Invalid issue" }, { status: 404 });
 
   const updateIssue = await prisma.issue.update({
-    where: {id: issue.id},
+    where: { id: issue.id },
     data: {
       title: body.title,
       description: body.description,
-    }
-  })
+    },
+  });
 
-  return NextResponse.json(updateIssue, { status: 200 })
+  return NextResponse.json(updateIssue, { status: 200 });
+}
+
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+  if (!issue)
+    return NextResponse.json({ error: "Invalid issue" }, { status: 404 });
+
+  await prisma.issue.delete({
+    where: { id: issue.id },
+  });
+
+  return NextResponse.json({})
 }
