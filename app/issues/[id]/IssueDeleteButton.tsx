70c42ff5d8b8e47af565a20965a96b09c56a417c
stkmukund <164354566+stkmukund@users.noreply.github.com>
2024-07-16 15:39:11 +0530

    Confirmation dialog box

--- app/issues/[id]/IssueDeleteButton.tsx
+++ app/issues/[id]/IssueDeleteButton.tsx
@@ -1,9 +1,26 @@
-import { Button } from '@radix-ui/themes'
+import { TrashIcon } from '@radix-ui/react-icons'
+import { AlertDialog, Button, Flex } from '@radix-ui/themes'
 import React from 'react'
 
 const IssueDeleteButton = ({ issueId }: { issueId: number }) => {
     return (
-        <Button color='red' >Delete Issue</Button>
+        <AlertDialog.Root>
+            <AlertDialog.Trigger>
+                <Button color='red' > <TrashIcon /> Delete Issue</Button>
+            </AlertDialog.Trigger>
+            <AlertDialog.Content>
+                <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
+                <AlertDialog.Description>Are you sure you want to delete this issue?</AlertDialog.Description>
+                <Flex mt='4' gap='4'>
+                    <AlertDialog.Cancel>
+                        <Button variant='soft' color='gray' >Cancel</Button>
+                    </AlertDialog.Cancel>
+                    <AlertDialog.Action>
+                        <Button color='red' >Confirm</Button>
+                    </AlertDialog.Action>
+                </Flex>
+            </AlertDialog.Content>
+        </AlertDialog.Root>
     )
 }
 
