ec26c6333b8d538e56d456a5f11cdb11e23b6d9f
stkmukund <164354566+stkmukund@users.noreply.github.com>
2024-07-17 16:32:42 +0530

    Refactor NavBar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -8,53 +8,63 @@ import { usePathname } from 'next/navigation';
 import { FaBug } from "react-icons/fa";
 
 const NavBar = () => {
+
+    return (
+        <nav className='p-5 border-b font-semibold'>
+            <Container>
+                <Flex justify='between' >
+                    <NavLinks />
+                    <AuthLinks />
+                </Flex>
+            </Container>
+        </nav>
+    )
+}
+
+const AuthLinks = () => {
+    const { status, data: session } = useSession();
+    if (status === 'loading') return null;
+    if (status === 'unauthenticated') return <Link href='/api/auth/signin' className='nav-link' >Log in</Link>;
+
+    return (
+        <Box>
+            <DropdownMenu.Root>
+                <DropdownMenu.Trigger>
+                    <Avatar src={session!.user?.image!} fallback='?' size='3' radius='full' className='cursor-pointer' referrerPolicy='no-referrer' />
+                </DropdownMenu.Trigger>
+                <DropdownMenu.Content>
+                    <DropdownMenu.Label>
+                        <Text size='3' >
+                            {session!.user?.email}
+                        </Text>
+                    </DropdownMenu.Label>
+                    <DropdownMenu.Item>
+                        <Link href='/api/auth/signout'>Log out</Link>
+                    </DropdownMenu.Item>
+                </DropdownMenu.Content>
+            </DropdownMenu.Root>
+        </Box >
+    )
+}
+
+const NavLinks = () => {
     const links = [
         { label: "Dashboard", href: "/" },
         { label: "Issues", href: "/issues/list" },
     ];
 
     const pathName = usePathname();
-
-    const { status, data: session } = useSession();
-    // console.log(session?.user?.image);
-
     return (
-        <nav className='p-5 border-b font-semibold'>
-            <Container>
-                <Flex justify='between' >
-                    <Flex align='center' gap='5' ><Link href='/' ><FaBug /></Link>
-                        <ul className='space-x-5 flex'>
-                            {links.map(link => <li key={link.href} > <Link href={link.href} className={classname(
-                                {
-                                    'text-zinc-900': pathName === link.href,
-                                    'text-zinc-500': pathName !== link.href,
-                                    'hover:text-zinc-800 transition-colors': true
-                                }
-                            )} >{link.label}</Link></li>)}
-                        </ul></Flex>
-                    <Box>
-                        {status === 'authenticated' && (
-                            <DropdownMenu.Root>
-                                <DropdownMenu.Trigger>
-                                    <Avatar src={session.user?.image!} fallback='?' size='3' radius='full' className='cursor-pointer' referrerPolicy='no-referrer' />
-                                </DropdownMenu.Trigger>
-                                <DropdownMenu.Content>
-                                    <DropdownMenu.Label>
-                                        <Text size='3' >
-                                            {session.user?.email}
-                                        </Text>
-                                    </DropdownMenu.Label>
-                                    <DropdownMenu.Item>
-                                        <Link href='/api/auth/signout'>Log out</Link>
-                                    </DropdownMenu.Item>
-                                </DropdownMenu.Content>
-                            </DropdownMenu.Root>
-                        )}
-                        {status === 'unauthenticated' && <Link href='/api/auth/signin'>Log in</Link>}
-                    </Box>
-                </Flex>
-            </Container>
-        </nav>
+        <Flex align='center' gap='5' ><Link href='/' ><FaBug /></Link>
+            <ul className='space-x-5 flex'>
+                {links.map(link => <li key={link.href} > <Link href={link.href} className={classname(
+                    {
+                        '!text-zinc-900': pathName === link.href,
+                        'nav-link': true
+                    }
+                )} >{link.label}</Link></li>)}
+            </ul>
+        </Flex>
     )
 }
 
