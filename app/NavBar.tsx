3c3673d31fd8923267602ab0a194ae915abec4fc
stkmukund <164354566+stkmukund@users.noreply.github.com>
2024-07-17 16:41:25 +0530

    login loading skeleton

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import {Skeleton} from '@/app/components'
 import { Avatar, Box, Container, DropdownMenu, Flex, Text } from '@radix-ui/themes';
 import classname from 'classnames';
 import { useSession } from 'next-auth/react';
@@ -23,7 +24,7 @@ const NavBar = () => {
 
 const AuthLinks = () => {
     const { status, data: session } = useSession();
-    if (status === 'loading') return null;
+    if (status === 'loading') return <Skeleton width='3rem' />;
     if (status === 'unauthenticated') return <Link href='/api/auth/signin' className='nav-link' >Log in</Link>;
 
     return (
